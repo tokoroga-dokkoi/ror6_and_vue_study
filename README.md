540ed89b50de8806460f5c50dcb492ccaf08a84d
MikiWaraMiki <a.i.u.e.o.1.2.3.a@gmail.com>
2019-11-23 17:13:43 +0900

    初コミット 環境構築が終了

---
+++ README.md
@@ -0,0 +1,7 @@
+Rails and Vue Studing Repository
+===
+RoR6とVue.jsの勉強用リポジトリ
+
+## Description
+- Ruby on Rails6(API)とVue.jsを利用し、ランニンングが趣味の人のシェアアプリを作成
+
---
+++ containers/rails/config/puma.rb
@@ -0,0 +1,24 @@
+# Puma can serve each request in a thread from an internal thread pool.
+# The `threads` method setting takes two numbers: a minimum and maximum.
+# Any libraries that use thread pools should be configured to match
+# the maximum value specified for Puma. Default is set to 5 threads for minimum
+# and maximum; this matches the default thread size of Active Record.
+#
+max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
+threads min_threads_count, max_threads_count
+
+# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
+#
+port        ENV.fetch("PORT") { 3000 }
+
+# Specifies the `environment` that Puma will run in.
+#
+environment ENV.fetch("RAILS_ENV") { "development" }
+plugin :tmp_restart
+
+
+app_root = File.expand_path("../../", __FILE__)
+bind "unix://#{app_root}/tmp/sockets/puma.sock"
+
+stdout_redirect "#{app_root}/log/puma.stdout.log", "#{app_root}/log/puma.stderr.log", true
