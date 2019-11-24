77caf04081d59e833b4b155bbc316a1c4c39945b
MikiWaraMiki <a.i.u.e.o.1.2.3.a@gmail.com>
2019-11-24 11:54:58 +0900

    [add]トップ画面を作成
    サイト閲覧時のトップ画面とHeaderを作成しました

---
+++ public-data/src/components/HelloRunningShare.vue
@@ -0,0 +1,67 @@
+<template>
+    <v-container fruid id="container-full">
+        <v-layout>
+            <v-flex xs12>
+                <v-img
+                    :src="require('../assets/index-prod.jpg')"
+                    gradient="to top right, rgba(105,105,105,.7), rgba(169, 169, 169, .4)"
+                >
+                    <v-row align="top" class="lightbox white--text pa-2 fill-height">
+                        <v-col>
+                            <div class="title px-2">
+                                発見があればランニングはもっと楽しくなる
+                            </div>
+                            <div class="subtitle-1">
+                                ランニンング中に見つけたお店・風景のシェアアプリ
+                            </div>
+                        </v-col>
+                    </v-row>
+                </v-img>
+            </v-flex>
+        </v-layout>
+        <v-layout mt-5 px-3 py-3 justify-center id="about">
+            <v-flex xs12>
+                <span class="display-2 font-weight-bold">About</span>
+            </v-flex>
+        </v-layout>
+        <v-layout mt-3 px-5 class="text-xs-center" row wrap id="about">
+            <v-flex lg4 xs12 >
+                <v-avatar size="250" color="grey lighten-4">
+                    <v-img
+                        :src="require('../assets/app-shoze.svg')"
+                    >
+                    </v-img>
+                </v-avatar>
+                <div class="my-2">
+                    <p class="font-weight-bold">ランニンング中に見つけた場所・風景をシェア</p>
+                </div>
+            </v-flex>
+            <v-flex lg4 xs12>
+                <v-avatar size="250" color="grey lighten-4">
+                    <v-img
+                        :src="require('../assets/app-map.svg')"
+                    >
+                    </v-img>
+                </v-avatar>
+                <div class="my-2">
+                    <p class="font-weight-bold">たまたま見つけたお店の場所を記録しておく</p>
+                </div>
+            </v-flex>
+            <v-flex lg4 xs12>
+                <v-avatar size="250" color="gray lighten-4">
+                    <v-img
+                        :src="require('../assets/app-sns.svg')">
+                    </v-img>
+                </v-avatar>
+                <div class="my-2">
+                    <p class="font-weight-bold">疑問や見つけたお店の情報に関して共有</p>
+                </div>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+<style lang="scss" scoped>
+#about{
+    text-align: center;
+}
+</style>
---
+++ public-data/src/components/common/Header.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <v-navigation-drawer
+            v-model="drawer"
+            absolute
+            temporary
+        >
+            <v-list dense>
+                <v-list-item
+                    v-for="item in items"
+                    :key="item.title"
+                >
+                    <v-list-item-icon>
+                        <v-icon>{{ item.icon }}</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                        <v-list-item-title>{{ item.title }}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+
+            </v-list>
+        </v-navigation-drawer>
+        <v-toolbar dark color="primary" fixed>
+            <v-app-bar-nav-icon
+                @click.stop="drawer = !drawer"
+                class="hidden-md-and-up"
+            >
+            </v-app-bar-nav-icon>
+            <v-toolbar-title class="headline text-uppercase white--text">
+                <span class="font-weight-light">Run Is Fun!!</span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-toolbar-items class="hidden-sm-and-down">
+                <v-btn mx-2 px-2 text>Home</v-btn>
+                <v-btn mx-2 px-2 text>Login</v-btn>
+                <v-btn mx-2 px-2 text>Sign Up</v-btn>
+            </v-toolbar-items>
+        </v-toolbar>
+    </div>
+</template>
+<script>
+export default {
+    name: "Header",
+    data: () => ({
+        drawer: false,
+        items: [
+            { title: 'Home', icon: "mdi-home-city" },
+            { title: 'Login', icon: "mdi-account-check" },
+            { title: 'SignUp', icon: "mdi-account-plus" }
+        ]
+    })
+}
+</script>
